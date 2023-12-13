24025948d0d0e4b77bad183c92258159c0c39b3e
coder chauhan <87173614+itsAnkitcode001@users.noreply.github.com>
2023-12-14 00:11:01 +0530

    Update calculatorTest.java

--- src/test/java/calculatorTest.java
+++ src/test/java/calculatorTest.java
@@ -13,7 +13,7 @@ public class calculatorTest {
 	}
 	
 	@Test
-	public void addTwoNumbers() {assertEquals(20, calc.add(10, 10));}
+	public void addTwoNumbers0() {assertEquals(20, calc.add(10, 10));}
 	@Test
 	public void addTwoNumbers1() {assertEquals(20, calc.add(10, 10));}
 	@Test
@@ -34,6 +34,46 @@ public class calculatorTest {
 	public void addTwoNumbers9() {assertEquals(20, calc.add(10, 10));}
 	@Test
 	public void addTwoNumbers10() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers11() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers12() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers13() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers14() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers15() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers16() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers17() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers18() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers19() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers20() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers21() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers22() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers23() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers24() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers25() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers26() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers27() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers28() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers29() {assertEquals(20, calc.add(10, 10));}
+	@Test
+	public void addTwoNumbers30() {assertEquals(20, calc.add(10, 10));}
 
 	@Test
 	public void addThreeNumbers() {
@@ -48,12 +88,84 @@ public class calculatorTest {
 
 	@Test
 	public void subtractTwoNumbers() {
+		assertEquals(0, calc.subtract(10, 10));}
+		@Test
+	public void subtractTwoNumbers34() {
+		assertEquals(0, calc.subtract(10, 10));}
+	@Test
+	public void subtractTwoNumbers35() {
+		assertEquals(0, calc.subtract(10, 10));}
+	@Test
+	public void subtractTwoNumbers36() {
+		assertEquals(0, calc.subtract(10, 10));}
+	@Test
+	public void subtractTwoNumbers37() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers38() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers39() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers40() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers41() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers42() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers43() {
+		assertEquals(0, calc.subtract(10, 10));
+		
+	}
+	@Test
+	public void subtractTwoNumbers44() {
 		assertEquals(0, calc.subtract(10, 10));
 		
 	}
 
 	@Test
-	public void subtractThreeNumbers() {
+	public void subtractThreeNumbers45() {
+		assertEquals(0, calc.subtract(20, 10, 10));
+		
+	}
+	@Test
+	public void subtractThreeNumbers46() {
+		assertEquals(0, calc.subtract(20, 10, 10));
+		
+	}
+	@Test
+	public void subtractThreeNumbers47() {
+		assertEquals(0, calc.subtract(20, 10, 10));
+		
+	}
+	@Test
+	public void subtractThreeNumbers48() {
+		assertEquals(0, calc.subtract(20, 10, 10));
+		
+	}
+	@Test
+	public void subtractThreeNumbers49() {
+		assertEquals(0, calc.subtract(20, 10, 10));
+		
+	}
+	@Test
+	public void subtractThreeNumbers50() {
 		assertEquals(0, calc.subtract(20, 10, 10));
 		
 	}
